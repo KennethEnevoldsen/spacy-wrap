7b2eb648424881c9d236d31097267462650a73fa
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-01-30 21:29:28 +0100

    updated long description from pypi

--- spacy_wrap/about.py
+++ spacy_wrap/about.py
@@ -1,4 +1,4 @@
 __title__ = "spacy-wrap"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __download_url__ = "https://github.com/kennethenevoldsen/spacy-wrap"
 __documentation__ = "https://kennethenevoldsen.github.io/spacy-wrap"
