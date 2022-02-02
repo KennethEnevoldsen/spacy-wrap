99abb15fd507314f4b96dec38a371448ed8e3d82
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-02-02 14:28:50 +0100

    fixed syntax error

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup, find_packages
 
 
 with open("spacy_wrap/about.py") as f:
-     v = f.read()
-     for l in v.split("\n"):
-         if l.startswith("__version__"):
-             __version__ = l.split('"')[-2]
+    v = f.read()
+    for l in v.split("\n"):
+        if l.startswith("__version__"):
+            __version__ = l.split('"')[-2]
 
 
- def setup_package():
+def setup_package():
     setup(version=__version__)
-
 
 
 if __name__ == "__main__":
