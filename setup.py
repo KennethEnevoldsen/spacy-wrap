0d504a6ce93a955d095e52f4902aeb839d8d6cdc
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-02-02 10:51:44 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 from setuptools import setup, find_packages
 
 
-with open("spacy_wrap/about.py") as f:
-    v = f.read()
-    for l in v.split("\n"):
-        if l.startswith("__version__"):
-            __version__ = l.split('"')[-2]
-
-
 def setup_package():
-    setup(name="spacy-wrap", version=__version__, packages=find_packages())
+    setup()
 
 
 if __name__ == "__main__":
