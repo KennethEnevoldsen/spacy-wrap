3210098c506b28de0de8a95b49260c726bba8539
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-02-02 14:22:54 +0100

    version to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
 
-def setup_package():
-    setup()
+with open("spacy_wrap/about.py") as f:
+     v = f.read()
+     for l in v.split("\n"):
+         if l.startswith("__version__"):
+             __version__ = l.split('"')[-2]
+
+
+ def setup_package():
+     setup(version=__version__)
+
 
 
 if __name__ == "__main__":
